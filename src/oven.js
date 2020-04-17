3fafd10ab816fcf4d8ae00bd35956d18f673c7a3
Petar Ivanov <pepincho95@gmail.com>
2020-04-17 15:06:11 +0300

    Add auto-heating functionality

--- src/oven.js
+++ src/oven.js
@@ -13,36 +13,37 @@ class Oven {
     _MAX_COOKING_TEMPERATURE = 240;
 
     constructor() {
-        this.heating_element = false;
         this._temperature = 0;
+        this._automaticHeater = false;
+
+        this.heating_element = false;
 
         machineEvents.on("ovenOn", this.on.bind(this));
         machineEvents.on("ovenOff", this.off.bind(this));
 
         machineEvents.on("pulse", this.performAction.bind(this));
         machineEvents.on("pulseOven", this.performAction.bind(this));
+
+        machineEvents.on("_ovenAutoHeater", this._autoHeater.bind(this));
     }
 
     performAction() {
         console.log("\n Oven >> performAction ...");
 
         if (ConveyorBelt[3]) {
-            ConveyorBelt[3] += "bake..3..";
+            ConveyorBelt[3] += `bake..${this._temperature}..`;
         }
 
         if (ConveyorBelt[4]) {
-            ConveyorBelt[4] += "bake..4..";
+            ConveyorBelt[4] += `bake..${this._temperature}..`;
         }
-    }
-
-    temperature() {
-        console.log("\n Current temperature: ", this._temperature);
     }
 
     async on() {
         console.log("\n >> Oven has been turned ON.");
 
         this.heating_element = true;
+        this._automaticHeater = true;
 
         this._startHeater()
         .then(() => {
@@ -50,6 +51,8 @@ class Oven {
                 console.log("\n ... Oven is ready.");
 
                 machineEvents.emit("ovenReady");
+
+                machineEvents.emit("_ovenAutoHeater");
             }
         });
     }
@@ -58,32 +61,58 @@ class Oven {
         console.log("\n >> Oven has been turned OFF.");
 
         this.heating_element = false;
+        this._automaticHeater = false;
 
         this._stopHeater();
     }
 
     async _startHeater() {
-        // while (this.heating_element) {
         while (this.heating_element && this._temperature < this._MAX_COOKING_TEMPERATURE) {
-            // if (this._temperature === this._MAX_COOKING_TEMPERATURE) {
-            //     this.off();
-            // }
-
             this._temperature += 5;
 
-            await sleep(500);
+            await sleep(1500);
+
+            if (this._temperature === this._MAX_COOKING_TEMPERATURE) {
+                this.heating_element = false;
+
+                machineEvents.emit("_ovenAutoHeater");
+            }
         }
     }
 
     async _stopHeater() {
-        while (!this.heating_element && this._temperature > this._ZERO_COOKING_TEMPERATURE) {
-            // if (this._temperature === this._MIN_COOKING_TEMPERATURE) {
-            //     this.on();
-            // }
+        while (!this.heating_element) {
+            if (this._automaticHeater && this._temperature === this._MIN_COOKING_TEMPERATURE) {
+                this.heating_element = true;
 
-            this._temperature -= 5;
+                machineEvents.emit("_ovenAutoHeater");
 
-            await sleep(500);
+                continue;
+            }
+
+            if (!this._automaticHeater && this._temperature > this._ZERO_COOKING_TEMPERATURE) {
+                this._temperature -= 5;
+            }
+
+            if (this._automaticHeater && this._temperature > this._MIN_COOKING_TEMPERATURE) {
+                this._temperature -= 5;
+            }
+
+            await sleep(1500);
+        }
+    }
+
+    async _autoHeater() {
+        if (this._automaticHeater) {
+            if (this._temperature === this._MAX_COOKING_TEMPERATURE) {
+                this.heating_element = false;
+                await this._stopHeater();
+            }
+
+            if (this._temperature === this._MIN_COOKING_TEMPERATURE) {
+                this.heating_element = true;
+                await this._startHeater();
+            }
         }
     }
 }
