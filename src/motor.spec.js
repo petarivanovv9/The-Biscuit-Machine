e30fdb3531d5020abf2a3581fde7bf97d9f01028
Petar Ivanov <pepincho95@gmail.com>
2020-05-06 13:46:35 +0300

    Add tests for the Motor's start method

--- src/motor.spec.js
+++ src/motor.spec.js
@@ -4,6 +4,10 @@ const { machineEvents } = require('./events');
 
 const {
     PULSE_EVENT,
+    PULSE_STAMPER_EVENT,
+    PULSE_OVEN_EVENT,
+
+    OVEN_OFF_EVENT,
 } = require('./constants');
 
 
@@ -60,6 +64,51 @@ describe('MotorDevice', () => {
             motorDevice.pulse();
 
             expect(machineEvents.emit).toHaveBeenCalledWith(PULSE_EVENT);
+        });
+    });
+
+    describe('_start', () => {
+        it('should execute a pulse, machine is on', () => {
+            const motorDevice = new Motor();
+            motorDevice._is_working = true;
+
+            motorDevice.pulse = jest.fn();
+
+            motorDevice._start();
+
+            expect(motorDevice.pulse).toHaveBeenCalledTimes(1);
+        });
+
+        it('should send pulse to stamper and oven, machine has been turned off', () => {
+            const motorDevice = new Motor();
+            motorDevice._is_working = true;
+            motorDevice._should_stop_motor = true;
+            motorDevice._should_send_pulse_stamper = true;
+
+            motorDevice.pulse = jest.fn();
+
+            machineEvents.emit = jest.fn();
+
+            motorDevice._start();
+            motorDevice._start();
+
+            expect(motorDevice.pulse).toHaveBeenCalledTimes(0);
+
+            expect(machineEvents.emit).toHaveBeenNthCalledWith(1, PULSE_STAMPER_EVENT);
+            expect(machineEvents.emit).toHaveBeenNthCalledWith(2, PULSE_OVEN_EVENT);
+            expect(machineEvents.emit).toHaveBeenNthCalledWith(3, PULSE_OVEN_EVENT);
+        });
+    });
+
+    describe('_stopMotorAndOven', () => {
+        it('should execute a pulse, machine is on', () => {
+            const motorDevice = new Motor();
+
+            machineEvents.emit = jest.fn();
+
+            motorDevice._stopMotorAndOven();
+
+            expect(machineEvents.emit).toHaveBeenCalledWith(OVEN_OFF_EVENT);
         });
     });
 });
